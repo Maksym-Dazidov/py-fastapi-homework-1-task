970f31e78082c698b8a63c7fe00b7811b3c8ff00
Maksym Dazidov <maksymdazidov@gmail.com>
2026-05-04 17:32:25 +0200

    implement schemas and endpoints for movie model

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -7,4 +7,11 @@ from database import get_db, MovieModel
 
 router = APIRouter()
 
-# Write your code here
+@router.get("/movies/", response_model=List[schemas.MovieListResponseSchema])
+async def get_movies(skip: int, limit: int, db: AsyncSession = Depends(get_db)):
+    return await get_movies(skip=skip, limit=limit, db=db)
+
+@router.get("/movies/{id}", response_model=schemas.MovieDetailResponseSchema)
+async def get_movie(id: int, db: AsyncSession = Depends(get_db)):
+    return await get_movie(id=id, db=db)
+
